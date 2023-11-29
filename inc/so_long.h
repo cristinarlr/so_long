f65fd75fcb67c65b2cb9b3e6b88f094799fea1cb
Cristina Ramirez lopez-ramallal <crramire@c1r15s2.42madrid.com>
2023-11-29 12:50:41 +0100

    setting up code environment

---
+++ inc/so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crramire <crramire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 12:45:25 by crramire          #+#    #+#             */
+/*   Updated: 2023/11/29 12:48:51 by crramire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef SO_LONG_H
+#define SO_LONG_H
+
+
+/* ---------------- PATH ---------------- */
+# include "../src/Libft/include/libft.h"
+# include "../src/ft_printf/include/ft_printf.h"
+# include "../src/mlx/include/mlx.h"
+/*# include "../src/Gnl/include/get_next_line.h"*/
+
+#endif
