d5f9bf294d9e0fbe47949c7bc1fcef98b01e28eb
Cristina Ramirez lopez-ramallal <crramire@c1r4s1.42madrid.com>
2023-12-11 12:02:28 +0100

    testing mlx functions

--- src/so_long.c
+++ src/so_long.c
@@ -1,5 +1,6 @@
 /*#include <mlx.h>*/
 #include <stdio.h>
+#include <stdlib.h>
 #include "../inc/so_long.h"
 #include "../mlx/mlx.h"
 
@@ -11,7 +12,72 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	int		key_pressed;
+	struct	timeval key_press_time;
+}	t_vars;
+
+int resize(int width, int height, t_vars *vars)
+{
+	(void)	width;
+	(void)	height;
+	(void)	vars;
+
+	printf("Window resized: %dx%d\n", width, height);
+	return(0);
+	
+}
+
+int	close_window(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit(0);
+	return(0);
+}
+
+int close(int keycode, t_vars *vars)
+{
+	if(keycode == 53)
+		close_window(vars);
+	return(0);
+}
+
+int	key_press(int keycode, t_vars *vars)
+{
+	vars->key_pressed = keycode;
+	gettimeofday(&(vars->key_pressed), NULL);
+	return(0);
+}
+
+int	key_release(int keycode, t_vars *vars)
+{
+	if(keycode == vars->key_pressed)
+	{
+		struct timeval current_time;
+		gettimeofdaay(&current_time, NULL);
+		double elaapsed_time = (current_time.tv_sec - vars->key_press_time.tv_sec) + (current_time.tv_usec - vars -> key_press_time.tv_usec) / 1000000.0;
+		if(elaapsed_time > 2.0)
+			printf("Key %d pressed for %.2f seconds\n", keycode, elaapsed_time);
+	}
+}
+
+int main(void)
+{
+		t_vars vars;
+
+		vars.mlx = mlx_init();
+		vars.win = mlx_new_window(vars.mlx, 1920, 1080, "MY GAME!");
+		mlx_hook(vars.win, 2, 1L<<0, close, &vars); //key press event
+		mlx_hook(vars.win, 17, 1L<<17, resize, &vars); //Window resize event
+		mlx_hook(vars.win, 17, 1L<<17, close_window, &vars); //Window close event
+		mlx_loop_hook(vars.mlx, loop_hook, &vars); //Continuous loop hook
+		mlx_loop(vars.mlx);
+}
+
+/*void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -22,9 +88,9 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 	printf("color: %d\n", color);
 	printf("dst: %s\n", dst);
-}
+}*/
 
-int	main(void)
+/*int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
@@ -38,4 +104,4 @@ int	main(void)
 	//mlx_pixel_put(mlx, mlx_win, 5, 5, 0x00ff0000);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
-}
+}*/
