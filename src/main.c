ba93c52d68a46fb5c430f84cac1d0b83c82a461e
Cristina Ramirez lopez-ramallal <crramire@c2r1s6.42madrid.com>
2023-12-14 13:37:57 +0100

    maps created

--- src/main.c
+++ src/main.c
@@ -9,7 +9,11 @@ int main (int argc, char **argv)
     map = NULL;
     i = argc;
     if (argc == 2)
+    {
+        map = malloc(sizeof(t_map));
         read_map(argv, map);
+        free(map);
+    }
     else
         return(-1);
     return(0);
--- src/read_map.c
+++ src/read_map.c
@@ -1,18 +1,78 @@
 #include "../inc/get_next_line.h"
 #include "../inc/so_long.h"
 
+static int map_raws(char **argv, int fd)
+{
+    int i;
+    char *line;
+
+    i = 0;
+    fd = open(argv[1], O_RDONLY);
+    while((line = get_next_line(fd)) != 0)
+    {
+        i++;
+        printf("line = %s", line);
+    }
+    printf("\ni = %i\n", i);
+    return(i);
+}
+
+void load_map(t_map *map, int fd)
+{
+    int i;
+    int raw;
+
+    printf("dentro de load_map\n");
+    printf("map->map_raw_count = %i\n", map->map_raw_count);
+    i = 0;
+    raw = map->map_raw_count;
+    while(raw > 0)
+    {
+        map->map[i] = get_next_line(fd);
+        printf("map[%i] = %s", i, map->map[i]);
+        i++;
+        raw--;
+    }
+}
+
+void copy_map(t_map *map)
+{
+    int i;
+
+    printf("\ndentro de copy_map\n");
+    i = 0;
+    map->map_cpy = malloc(sizeof(char **)*map->map_raw_count);
+    while(i < (map->map_raw_count))
+    {
+        map->map_cpy[i] = strdup(map->map[i]);
+        printf("map->map_cpy[%i] = %s", i, map->map_cpy[i]);
+        i++;
+    }
+}
+
 int read_map(char **argv, t_map *map)
 {
     int fd;
+    int raws;
    
     printf("dentro de read_map\n");
-    map->map_raw_count = 0;
-    printf("1\n");
-    fd = open(argv[map->map_raw_count], O_RDONLY);
-    while((map->map[map->map_raw_count]= get_next_line(fd)) != 0)
-    {
-        printf("%s", map->map[map->map_raw_count]);
-    }
+    fd = open(argv[1], O_RDONLY);
+    //sacar el tamaño del array map_size
+    raws = map_raws(argv, fd);
+    map = malloc(sizeof(t_map));
+    //inicializar el array con el tamaño
+    map->map = malloc(sizeof(char **)*raws);
+    if(!map->map)
+        return(-1);
+    map->map_raw_count = raws;
+    //almacenar el mapa
+    load_map(map, fd);
+    //revisar que es cuadrado
+    //parsear para chequeo
+    //generar copia del mapa (porque ya está OK)
+    copy_map(map);
+    
+    
     return(0);
 }
  
